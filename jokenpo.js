f2e9b8461c376d7a3927fe41649b3b5b5b711758
IcaroD <icaro.davila.santos@gmail.com>
2023-07-14 22:47:25 -0300

    Começo de um pedra, papel e tesoura
    Começando um projeto de jogo

---
+++ jokenpo.js
@@ -0,0 +1,44 @@
+var pedra = false 
+var papel = false
+var tesoura = false
+
+function selectTesoura(){
+    tesoura = true;
+    pedra = false;
+    papel = false;
+}
+
+function selectPapel(){
+    tesoura = false;
+    pedra = false;
+    papel = true;
+}
+
+function selectPedra(){
+    tesoura = false;
+    pedra = true;
+    papel = false;
+}
+
+var resultadoSorteio = 0;
+var random = Math.floor(Math.random() * 2);
+
+if (random == 0){
+    resultadoSorteio = 'tesoura';
+}
+else if (random == 1){
+    resultadoSorteio = 'papel'
+}
+else{
+    resultadoSorteio = 'pedra'
+}
+
+if ((pedra == true && resultadoSorteio == 'tesoura') || (tesoura == true && resultadoSorteio == 'papel') || (papel == true && resultadoSorteio == 'pedra')){
+    document.getElementById("ganhador").innerHTML = "Usuário venceu."
+}
+else if ((papel == true && resultadoSorteio == 'tesoura') || (pedra == true && resultadoSorteio == 'papel') || (tesoura == true && resultadoSorteio == 'pedra')){
+    document.getElementById("ganhador").innerHTML = "Computador venceu."
+}
+else ((tesoura == true && resultadoSorteio == 'tesoura') || (papel == true && resultadoSorteio == 'papel') || (pedra == true && resultadoSorteio == 'pedra')){
+    document.getElementById("ganhador").innerHTML = "Houve empate."
+}
